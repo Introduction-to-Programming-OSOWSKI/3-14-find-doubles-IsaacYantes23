165ff7c2f7e0f476a736cc0b6ecc7c7b85abc807
IsaacYantes23 <817296@mystma.org>
2021-02-09 17:01:46 +0000

    foind doubles try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def findDoubles(w):
+    for i in range(0, len(w)):
+        if w[i] == w[i-1]:
+            return True
+
+    else:
+        return False
+
+print(findDoubles("madagascar"))
+
